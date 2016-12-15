15775c5bfc8c0115f76bdc401ca399b3520c8619
AileenLumina <aileenfromthemoon@gmail.com>
2016-12-15 03:05:45 +0100

    The 'utilities' entry was pointing to this repository; it is now pointing to its repository.

--- __init__.py
+++ __init__.py
@@ -3,7 +3,7 @@ index = {
         'name': 'Utilities',
         'description': 'Various useful utilities.',
         'long_description': 'A number of tools that may make the life of a bot admin easier.',
-        'repository': 'https://github.com/Dwarf-Community/Dwarf-Extensions',
+        'repository': 'https://github.com/Dwarf-Community/Dwarf-Utilities',
         'author': 'AileenLumina',
         'author_id': '172003635826393089',
     },
